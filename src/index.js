6e39a8a0767318780613b6cce0167bed1dddf4b1
mmehul2403 <mmehul2403@gmail.com>
2024-04-29 14:39:13 -0400

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <h1>Hello from dev-card</h1>;
+}
+
+//creating root in reactDOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//rendering App funct   ion in a strict mode
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
